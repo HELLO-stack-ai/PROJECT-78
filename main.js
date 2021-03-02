fcf00228c08310de35231a40d6d00580b722b2ee
HELLO-stack-ai <79909814+HELLO-stack-ai@users.noreply.github.com>
2021-03-02 16:32:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var images=[
+"WhatsApp Image 2021-02-28 at 3.19.40 PM.jpeg",
+"NANA PHOTO.jpg",
+"NANI PHOTO.jpg",
+"TASLIM MAMA PHOTO.png",
+"BADI MAMI PHOTO.png",
+"ANUM PHOTO.jpg",
+"WhatsApp Image 2021-02-28 at 3.00.53 PM.jpeg",
+"CHOTI MAMI PHOTO.webp",
+"MUMMY PHOTO.png",
+"PAPA PHOTO.png"]
+ var i=0;
+function start(){
+if (i==10);
+  {
+    i=0;
+    document.getElementById("img1").src="images"[i]
+  i++;
+}
+}
